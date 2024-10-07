a113bc0b98ec89eb0a428c4aa9f1cd58857c1ebf
Alison <alisonsarto07@gmail.com>
2024-10-07 17:42:32 -0300

    Removendo a duplicidade da função send()

--- api/login/index.php
+++ api/login/index.php
@@ -1,16 +1,8 @@
 <?php
 
-	function send($message) {
-		header('Content-Type: application/json;');
-		http_response_code($message['status'] ?? 200);
-		echo json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-		exit;
-	}
+	include $_SERVER['DOCUMENT_ROOT'].'/server/db/connect.php';
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-		//? Faz login padrão
-		require $_SERVER['DOCUMENT_ROOT'].'/server/db/connect.php';
 
 		if (isset($_POST['email']) && isset($_POST['senha'])) {
 			
