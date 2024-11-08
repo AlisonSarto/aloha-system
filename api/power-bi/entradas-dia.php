a39357790986194984b9e9416d928bf4dd4f850c
Alison <alisonsarto07@gmail.com>
2024-11-08 17:25:31 -0300

    Possivel versão final para a os gráficos de Adm da produção

---
+++ api/power-bi/entradas-dia.php
@@ -0,0 +1,108 @@
+<?php
+
+  include $_SERVER['DOCUMENT_ROOT'].'/server/funcs/acess.php';
+  include $_SERVER['DOCUMENT_ROOT'].'/server/funcs/verificar-turno.php';
+  acessApi('dashboard', 'visualizar');
+
+  if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    send([
+      'status' => 405,
+      'message' => 'Método não permitido'
+    ]);
+  }
+
+  //? Puxa a data da entrada mais antiga
+  $sql = "SELECT * FROM entradas ORDER BY turno_dia ASC LIMIT 1";
+  $res = $conn->query($sql);
+  $db = $res->fetch_assoc();
+
+  $data_inicio = $db['turno_dia'];
+  
+  //? Faz um array de datas até a data atual
+  $data_atual = date('Y-m-d');
+  $dias = [];
+
+  $data = new DateTime($data_inicio);
+  $data_atual = new DateTime($data_atual);
+
+  while ($data <= $data_atual) {
+    $dias[] = $data->format('Y-m-d');
+    $data->modify('+1 day');
+  }
+
+  //? Puxa os turnos segundo as entradas
+  $sql = "SELECT DISTINCT turno FROM entradas";
+  $res = $conn->query($sql);
+  $turnos = $res->fetch_all(MYSQLI_ASSOC);
+
+  //? Mescla os arrays semanas e dias
+  $semana_dias = [];
+  foreach ($dias as $dia) {
+    $data = new DateTime($dia);
+    $semana = $data->format('W-Y');
+    $semana_dias[$semana][] = $data->format('Y-m-d');
+  }
+
+  //? Puxa todas as entradas
+  $sql = "SELECT * FROM entradas ORDER BY dia DESC";
+  $res = $conn->query($sql);
+  $entradas = $res->fetch_all(MYSQLI_ASSOC);
+  
+  //* Junta os valores
+  $res = [];
+  foreach ($entradas as $entrada) {
+
+    $dia = $entrada['turno_dia'];
+    $turno = $entrada['turno'];
+    $qtd = (int) $entrada['qtd'] ?? 0;
+
+    $res[$dia][$turno]['meta'] = (int)$entrada['meta'];
+    $res[$dia][$turno]['meta_dia'] = (int) ((int)$entrada['meta'] * $entrada['horas_de_trabalho']);
+
+    if ($res[$dia][$turno]['meta_dia'] >= 1) {
+      $res[$dia][$turno]['meta_dia']--;
+    }
+
+    if (empty($res[$dia][$turno]['qtd'])) {
+      $res[$dia][$turno]['qtd'] = $qtd;
+    }else {
+      $res[$dia][$turno]['qtd'] += $qtd;
+    }
+    
+  }
+
+  //* Formata o resultado
+  $data = [];
+  foreach ($semana_dias as $semana => $dias) {
+    foreach ($dias as $dia) {
+      foreach ($turnos as $turno) {
+
+        $turno = $turno['turno'];
+        if (empty($res[$dia][$turno])) {
+          $data[] = [
+            'semana' => $semana,
+            'dia' => $dia,
+            'turno' => $turno,
+            'qtd' => 0,
+            'meta_dia' => 0,
+          ];
+        }else {
+          $data[] = [
+            'semana' => $semana,
+            'dia' => $dia,
+            'turno' => $turno,
+            'qtd' => $res[$dia][$turno]['qtd'],
+            'meta_dia' => $res[$dia][$turno]['meta_dia'],
+          ];
+        }
+
+      }
+    }
+  }
+
+  send([
+    'status' => 200,
+    'data' => $data
+  ]);
+
+?>
---
+++ api/power-bi/entradas-hora.php
@@ -0,0 +1,118 @@
+<?php
+
+  include $_SERVER['DOCUMENT_ROOT'].'/server/funcs/acess.php';
+  include $_SERVER['DOCUMENT_ROOT'].'/server/funcs/verificar-turno.php';
+  acessApi('dashboard', 'visualizar');
+
+  if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    send([
+      'status' => 405,
+      'message' => 'Método não permitido'
+    ]);
+  }
+
+  //? Puxa a data da entrada mais antiga
+  $sql = "SELECT * FROM entradas ORDER BY turno_dia ASC LIMIT 1";
+  $res = $conn->query($sql);
+  $db = $res->fetch_assoc();
+
+  $data_inicio = $db['turno_dia'];
+  
+  //? Faz um array de datas até a data atual
+  $data_atual = date('Y-m-d');
+  $dias = [];
+
+  $data = new DateTime($data_inicio);
+  $data_atual = new DateTime($data_atual);
+
+  while ($data <= $data_atual) {
+    $dias[] = $data->format('Y-m-d');
+    $data->modify('+1 day');
+  }
+
+  //? Puxa os turnos segundo as entradas
+  $sql = "SELECT DISTINCT turno FROM entradas";
+  $res = $conn->query($sql);
+  $turnos = $res->fetch_all(MYSQLI_ASSOC);
+
+  //? Puxa todas as entradas
+  $sql = "SELECT * FROM entradas ORDER BY dia DESC";
+  $res = $conn->query($sql);
+  $entradas = $res->fetch_all(MYSQLI_ASSOC);
+  
+  //? Junta os valores
+  $res = [];
+  foreach ($entradas as $entrada) {
+
+    $data = $entrada['turno_dia'];
+    $turno = $entrada['turno'];
+
+    $qtd = (int) $entrada['qtd'];
+    $meta = (int) $entrada['meta'];
+
+    $hora = date('H:00', strtotime($entrada['dia']));
+    
+    $res[$data][$turno]['meta'] = $meta;
+
+    if (empty($res[$data][$turno]['entradas'][$hora])) {
+      $res[$data][$turno]['entradas'][$hora] = $qtd;
+    }else {
+      $res[$data][$turno]['entradas'][$hora] += $qtd;
+    }
+
+  }
+
+  //? Preenche os valores faltantes
+  foreach ($res as $data => $turnos) {
+    foreach ($turnos as $turno => $dados) {
+      $min = min(array_keys($dados['entradas']));
+      $max = max(array_keys($dados['entradas']));
+
+      $hora = new DateTime($min);
+      $hora_max = new DateTime($max);
+
+      while ($hora <= $hora_max) {
+        $hora = $hora->format('H:00');
+        if (empty($res[$data][$turno]['entradas'][$hora])) {
+          $res[$data][$turno]['entradas'][$hora] = 0;
+        }
+        $hora = new DateTime($hora);
+        $hora->modify('+1 hour');
+      }
+    }
+  }
+
+  //? Organiza em ordem
+  foreach ($res as $data => $turnos) {
+    foreach ($turnos as $turno => $dados) {
+      ksort($res[$data][$turno]['entradas']);
+    }
+  }
+
+  send([
+    'status' => 200,
+    'data' => $res
+  ]);
+
+  //* Formata a resposta para que fique varios objetos dentro de um array
+  $data_final = [];
+  foreach ($res as $data => $turnos) {
+    foreach ($turnos as $turno => $dados) {
+      foreach ($dados['entradas'] as $hora => $qtd) {
+        $data_final[] = [
+          'data' => $data,
+          'turno' => $turno,
+          'hora' => $hora,
+          'qtd' => $qtd,
+          'meta' => $dados['meta']
+        ];
+      }
+    }
+  }
+  
+  send([
+    'status' => 200,
+    'data' => $data_final
+  ]);
+
+?>
